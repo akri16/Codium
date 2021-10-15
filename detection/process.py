43599f30d06cd32a9b962bb0f5cd2f4747911765
Amit Krishna A <amit.ananthkumar@gmail.com>
2021-10-15 21:16:21 +0530

    Setup backend

--- detection/process.py
+++ detection/process.py
@@ -16,9 +16,9 @@ extractor = ember.features.PEFeatureExtractor()
 
 def predict(raw_bytes):
     feature = np.array(extractor.feature_vector(raw_bytes), dtype=np.float32)
-    
+
     features = np.array([feature[:2351]], dtype=np.float32)
     features = scaler.transform(features)
     features = np.expand_dims(features, axis=-1)
-    return model.predict(features)[0]
+    return model.predict(features)[0][-1].item()
 
